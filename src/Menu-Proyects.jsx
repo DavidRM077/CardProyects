92ee45a20854147b03a52778b247940940dd555e
DavidR <davidrap087@gmail.com>
2024-02-12 00:20:28 -0400

    agregando un boton para darle like a los proyectos

--- src/Menu-Proyects.jsx
+++ src/Menu-Proyects.jsx
@@ -1,8 +1,23 @@
 import './Estilo.css'
+import { Route,Link} from 'react-router-dom';
+import { useState } from 'react';
 
-function MenuProyects({PageName,TextName,imgUrl}){
+// eslint-disable-next-line react/prop-types
+function MenuProyects({PageName,TextName,imgUrl,InicialLike}){
+
+const [Likee,setLikee] = useState(InicialLike)
+const ButtonClaseName = Likee ? "Like" : "DisLike"
+    
+const Like = () => 
+{
+    setLikee(!Likee)
+}
+
     return(
 <article className="Proyects-Card">
+    <div className='Contenedor-Like'>
+        <span className={ButtonClaseName} onClick={Like}>   </span>
+    </div>
     <header>
         <img className='Proyects-Card-img' src={imgUrl} alt="" />
      </header>
@@ -12,7 +27,8 @@ function MenuProyects({PageName,TextName,imgUrl}){
      </div>
 
 <aside>
-    <button className='Code-Button'> Codigo </button>
+    
+    <button  className='Code-Button' >  Codigo </button>
     <button className='Visite-Buttom'> Visitar </button>
 </aside>
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MenuProyects from './Menu-Proyects'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MenuProyects from "./Menu-Proyects";
+import "./index.css";
 
-const Img1 = "/img/Ramirez & Asociados.jpg"
-const Img2 = "/img/Fundacion Reyes Jimenes.jpg"
-const Img3 = "/img/Encriptador.jpg"
+const Img1 = "/img/Ramirez & Asociados.jpg";
+const Img2 = "/img/Fundacion Reyes Jimenes.jpg";
+const Img3 = "/img/Encriptador.jpg";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const sitios = [
+  {
+    PageName: "Ramirez & Asociados",
+    TextName: "Pagina Web para Ramirez & Asociados donde contiene informacion sobre la empresa",
+    imgUrl: Img1,
+    InicialLike: true
+    
+  },
+  {
+    PageName: "Fundacion Reyes Jimenes",
+    TextName: "Pagina Web realizada para la fundación Reyes Jiménez",
+    imgUrl: Img2,
+    InicialLike: false
+  },
+  {
+    PageName: "Encriptador de Texto",
+    TextName: "Sistema que encripta textos y también puede descifrar textos y copiarlos",
+    imgUrl: Img3,
+    LinkCode: 'https://github.com/DavidRM077/Encriptador-de-texto',
+    InicialLike: true
+  },
+];
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
-    <section className='Card-Proyects'>
-    <MenuProyects PageName={"Ramirez & Asociados"} TextName={"Pagina Web para Ramirez & Asociados donde contiene informacion sobre la empresa "} imgUrl={Img1}/>
-
-    <MenuProyects  imgUrl={Img2} PageName={"Fundacion Reyes Jimenez"} TextName={"Pagina web desarrolalda para la fundacion Reyes Jimenez"}/>
     
-    <MenuProyects imgUrl={Img3} PageName={"Encriptador"} TextName={"Encriptador de Texto,Reto De Alura Latam"}/>
-
+    <br />
+    <section className="Card-Proyects">
+      {sitios.map(({ PageName, TextName, imgUrl,LinkCode,InicialLike }) => (
+        <MenuProyects key={PageName} PageName={PageName} TextName={TextName} imgUrl={imgUrl} LinkCode={LinkCode} InicialLike={InicialLike} />
+      ))}
     </section>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
